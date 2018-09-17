e90e022a8f0a7ddb1997d696a58d2758f7d3ccb4
Alex Lauderdale <alauderdale@Alexs-MacBook-Pro.local>
2018-09-17 01:32:12 -0600

    more

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
 
 	$( ".menu-button" ).click(function() {
@@ -8,11 +10,16 @@ $(document).ready(function(){
         $('.menu-button').trigger('click');
     });
 
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 5) {
+    	$("#primary-nav").removeClass("navbar-page-top");
+    }
 
 
 	$(window).scroll(function() {    
-	    var scroll = $(window).scrollTop();
-
+	    
+		var scroll = $(window).scrollTop();
 	    if (scroll >= 5) {
 	        $("#primary-nav").removeClass("navbar-page-top");
 	    } else {
@@ -25,6 +32,44 @@ $(document).ready(function(){
 	  var goto = Number( $(this).attr('data-slide-to') );
 	  $("#ToolsSLider").carousel(goto);  
 	});
+
+
+	// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 
 
 
